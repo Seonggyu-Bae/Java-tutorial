dbfe10577f6728b09a7647a790c389c50f22c97b
Seonggyu-Bae <45026759+Seonggyu-Bae@users.noreply.github.com>
2018-11-17 17:18:13 +0900

    Update and rename HW4_1_iiV2.java to HW4-1.java

---
+++ HW4/HW4-1.java
@@ -0,0 +1,13 @@
+public class HW4-1{
+	public static void main(String [] args){
+		int i, j;
+		for(i=2; i<100; i++){
+			for(j=2; j<i; j++){		
+				if(i%j==0) 
+					break;	
+			}
+			if(i == j) 
+				System.out.printf("%3d", i);				
+		}
+	}
+}
