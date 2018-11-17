1ebc018752d1cb1f38e7d98048826f91604f8281
Seonggyu-Bae <45026759+Seonggyu-Bae@users.noreply.github.com>
2018-11-17 16:53:21 +0900

    Update HW2_2.java

--- HW2/HW2_2.java
+++ HW2/HW2_2.java
@@ -14,18 +14,11 @@ class HW2_2{
 			sign = "양수";
 		}
 		else{
-			abs = -a; //abs = ~a+1
+			abs = -a;
 			sign = "음수";
 		}
-		
-		
-		
-		
-		
-		
-		
 		quo = abs>>2;
-		res = abs&0b11;		// 4로 나눈 나머지는 마지막 2비트(0, 1, 2,3 중 하나 )이기 때문에  0b11과 AND 시키면(마지막 2비트만 남음)알아낼 수 있다. 
+		res = abs&0b11;	// 4로 나눈 나머지는 마지막 2비트(0,1,2,3 중 하나)이기 때문에 0b11과 AND 시키면(마지막 2비트만 남음)알아낼 수 있다. 
 		
 		System.out.format("\"%d는 %s입니다. 이 수의 절댓값은 %d이고,\n", a, sign, abs);
 		System.out.format("\t이를 4로 나눈 몫은 %d이고,\n", quo);
